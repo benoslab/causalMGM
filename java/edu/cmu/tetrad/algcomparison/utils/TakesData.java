8bfefeab7a6216bb1c6b389186c764b15a9f2816
nehaabraham <nea24@pitt.edu>
2017-08-18 09:47:14 -0400

    remove java source code from package

--- java/edu/cmu/tetrad/algcomparison/utils/TakesData.java
+++
@@ -1,8 +0,0 @@
-package edu.cmu.tetrad.algcomparison.utils;
-
-/**
- * Tags a gadget that takes a data model list as argument to its constructor.
- * @author jdramsey
- */
-public interface TakesData {
-}
--- java/edu/cmu/tetrad/algcomparison/utils/TakesInitialGraph.java
+++
@@ -1,9 +0,0 @@
-package edu.cmu.tetrad.algcomparison.utils;
-
-/**
- * Tags an algorithm that can take an initial graph as input.
- *
- * @author jdramsey
- */
-public interface TakesInitialGraph {
-}
